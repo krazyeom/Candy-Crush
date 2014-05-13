59592ae463582d00b911d38fb4c3cd7b163d7a8a
Steve Yeom <krazyeom@gmail.com>
2014-05-13 20:35:10 +0900

    Initial Commit

---
+++ CookieCrunch/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  CookieCrunch
+//
+//  Created by Steve Yeom on 5/13/14.
+//  Copyright 2nd Jobs 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ CookieCrunch/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CookieCrunch
+//
+//  Created by Steve Yeom on 5/13/14.
+//  Copyright 2nd Jobs 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
